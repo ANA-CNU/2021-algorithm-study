9e5634cf20933d05f430908b8a92e1fa8351e26c
rlcz1 <86146265+rlcz1@users.noreply.github.com>
2021-08-05 01:19:17 +0900

    Add files via upload
    ╰（‵□′）╯

---
+++ 전규리/FindPassword_17219.java
@@ -0,0 +1,40 @@
+package boj;
+
+import java.util.*;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+import java.io.*;
+
+public class FindPassword_17219 {
+
+	public static void main(String[] args) throws IOException {		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		int sl = Integer.parseInt(st.nextToken());
+		int pl = Integer.parseInt(st.nextToken());
+		
+		ArrayList<String> _f = new ArrayList<>();
+		
+		HashMap<String, String> map = new HashMap<String, String>();
+		
+		for (int i = 0; i < sl; i++) {
+			String[] val = br.readLine().split(" ");
+			map.put(val[0], val[1]);
+		}
+		
+		for (int i = 0; i < pl; i++) {
+			_f.add(br.readLine());
+		}
+		
+		for (int i = 0; i < pl; i++) {
+			bw.write(map.get(_f.get(i))+"\n");
+		}
+		
+		bw.flush();
+		bw.close();
+	}
+
+}
