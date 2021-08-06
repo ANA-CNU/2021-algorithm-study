99b230e638c7b263c80c53fbf1ec768150ce3384
junghyeonsu <jung660317@naver.com>
2021-08-06 12:46:24 +0900

    백준 NodeJs 설정

---
+++ 정현수/solve.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+// 백준 제출
+// const input = fs.readFileSync('/dev/stdin').toString().split(' ');
+
+// VSC 테스트 용
+const input = fs.readFileSync('input.txt').toString().split(' ');
+
+console.log(input);
+
