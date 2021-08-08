be01d8515b4aff217dcc5cc2e4d73422f4fe65e4
junghyeonsu <54893898+junghyeonsu@users.noreply.github.com>
2021-08-08 22:17:38 +0900

    2021/08/08 문제 풀이

--- 정현수/solve.js
+++ 정현수/solve.js
@@ -6,12 +6,26 @@ const fs = require('fs');
 // VSC 테스트 할 때 주석 제거
 const readFileSyncAddress = 'input.txt';
 
-let input = fs.readFileSync(readFileSyncAddress).toString().trim().split(' ');
-input = Number(input[0]);
+let input = fs.readFileSync(readFileSyncAddress).toString().trim().split('\n');
 
 // 문제 풀이
 function solution(input) {
-    
+	[n, ...person] = input;
+	let count = [];
+
+	for (let i = 0; i < n; i++) {
+		let rank = 0;
+		for (let j = 0; j < n; j++) {
+			if (i === j) continue;
+			[aWeight, aTall] = person[i].split(' ');
+			[bWeight, bTall] = person[j].split(' ');
+
+			if (Number(aWeight) < Number(bWeight) && Number(aTall) < Number(bTall))
+				rank += 1;
+		}
+		count.push(rank + 1);
+	}
+	return count.join(' ');
 }
 
 // 제출
