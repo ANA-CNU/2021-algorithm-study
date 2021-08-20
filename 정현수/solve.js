d390cdcc6290679465881ef25103d980985d7a4a
junghyeonsu <jung660317@naver.com>
2021-08-20 15:59:48 +0900

    2021/08/20 문제 풀이

--- 정현수/solve.js
+++ 정현수/solve.js
@@ -7,28 +7,19 @@ const readFileSyncAddress = "input.txt";
 let input = fs.readFileSync(readFileSyncAddress).toString().trim().split("\n");
 
 // 인풋 처리
-[n, ...arr] = input;
+[n, arr] = input;
 n = Number(n);
-arr = arr.map((i) => i.split(" ").map((v) => Number(v)));
+arr = arr.split(" ").map((i) => Number(i));
 solution(n, arr);
 
 // 문제 풀이
 function solution(n, times) {
-	times = times.sort((a, b) => {
-		if (a[1] - b[1] === 0) {
-			return a[0] - b[0];
-		}
-		return a[1] - b[1];
-	});
-	let nowClass = times[0];
-	let answer = 1;
-	for (let i = 1; i < n; i++) {
-		const temp = times[i];
-		if (nowClass[1] <= temp[0]) {
-			nowClass = times[i];
-			answer += 1;
-		}
+	let answer = 0;
+	let temp = 0;
+	times = times.sort((a, b) => a - b);
+	for (let i = 0; i < n; i++) {
+		temp += times[i];
+		answer += temp;
 	}
-
 	console.log(answer);
 }
