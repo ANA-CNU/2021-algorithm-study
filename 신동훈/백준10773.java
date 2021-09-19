8bdd89cfe4efb3b8eb8e92c30346a12293c27d11
Dongun Shin <52229930+ShinDongHun1@users.noreply.github.com>
2021-09-20 06:05:00 +0900

    백준 10773번 제로(09/21)

---
+++ 신동훈/백준10773.java
@@ -0,0 +1,29 @@
+package 백준;
+
+import java.io.*;
+import java.util.Stack;
+
+public class 백준10773 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N =Integer.parseInt(br.readLine());
+
+        Stack<Integer> stack = new Stack<>();
+        for(int i=0; i<N; i++){
+            int num = Integer.parseInt(br.readLine());
+            if(num==0){
+                stack.pop();
+                continue;
+            }
+            stack.push(num);
+        }
+
+        int sum=0;
+        while(!stack.isEmpty()){
+            sum+=stack.pop();
+        }
+        bw.write(sum+"");
+        bw.flush();
+    }
+}
