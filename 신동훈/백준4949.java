7c34dc4fcdf96589d15152cd1925acba59d7cf52
Dongun Shin <52229930+ShinDongHun1@users.noreply.github.com>
2021-09-22 23:09:37 +0900

    백준 4949 /09/22

---
+++ 신동훈/백준4949.java
@@ -0,0 +1,65 @@
+package baekjoon;
+
+import java.io.*;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class 백준4949 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String s;
+        while(true){
+            s=br.readLine();
+
+            if(s.equals(".")){
+                break;
+            }
+
+            else{
+                Stack<String> stack =new Stack<>();
+                String [] str =  s.split("");
+
+                for(int i=0; i<str.length; i++){
+                    String temp = str[i];
+                    if(temp.equals(".")){
+                        break;
+                    }
+                    if(temp.equals("[") || temp.equals("(")){
+                        stack.push(temp);
+                    }
+                    else if(temp.equals("]")){
+                        if(stack.isEmpty() || !stack.peek().equals("[")){
+                            stack.push("]");
+                            break;
+                        }
+                        else if(stack.peek().equals("[")){
+                            stack.pop();
+                        }
+                    }
+                    else if(temp.equals(")")){
+                        if(stack.isEmpty()|| !stack.peek().equals("(")){
+                            stack.push(")");
+                            break;
+                        }
+                        else if(stack.peek().equals("(")){
+                            stack.pop();
+                        }
+                    }
+                }
+
+                if(stack.isEmpty()){
+                    System.out.println("yes");
+                }else{
+                    System.out.println("no");
+                }
+                stack.clear();
+            }
+
+        }
+
+    }
+
+
+}
