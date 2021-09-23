a2e53313667abc873bf55a1402f6fb22ae64fdf4
Dongun Shin <52229930+ShinDongHun1@users.noreply.github.com>
2021-09-23 21:30:29 +0900

    백준 1874, 21.09.23

---
+++ 신동훈/백준1874.java
@@ -0,0 +1,45 @@
+package baekjoon;
+
+import java.io.*;
+import java.util.Stack;
+
+public class 백준1874 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N =Integer.parseInt(br.readLine());//숫자의 개수.
+
+        int [] result = new int[N];//만들어야 할 순열
+
+        for(int  i=0; i<N; i++){ //1~N까지 입력받는다
+            result[i]=Integer.parseInt(br.readLine());
+        }
+
+        Stack<Integer> stack = new Stack<>();
+        int count = 0;
+
+        for(int  i=1; i<=N; i++){ //1~N까지 입력받는다
+
+            stack.push(i);//집어넣었을 때
+            sb.append("+\n");
+            while (stack.peek()==result[count]){
+                stack.pop();
+                sb.append("-\n");
+                count++;
+
+                if(stack.isEmpty()){
+                    break;
+                }
+            }
+
+        }
+        if(!stack.isEmpty()){
+            System.out.println("NO");
+        }else{
+            System.out.print(sb);
+        }
+
+    }
+}
