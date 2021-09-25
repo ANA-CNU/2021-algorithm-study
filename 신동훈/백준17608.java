f45724d67674fbb7811b80dc8749655793f4c690
Dongun Shin <52229930+ShinDongHun1@users.noreply.github.com>
2021-09-26 06:11:42 +0900

    백준17608 9/26

---
+++ 신동훈/백준17608.java
@@ -0,0 +1,27 @@
+package baekjoon;
+
+import java.io.*;
+import java.util.Stack;
+
+public class 백준17608 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        Stack<Integer> stack =new Stack<>();
+
+        for(int i=0; i<N; i++){
+            int temp = Integer.parseInt(br.readLine());
+
+            while(!stack.isEmpty() && stack.peek()<=temp){
+                stack.pop();
+            }
+            stack.push(temp);
+
+        }
+        System.out.println(stack.size());
+
+
+    }
+}
