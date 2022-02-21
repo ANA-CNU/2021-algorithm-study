f0ea667857e52121aa807fabc1fd243b1d16e983
dayo2n <0217dayun@naver.com>
2022-02-21 22:41:12 +0900

    BOJ 2631 : dp

---
+++ 문다연/Q2631.java
@@ -0,0 +1,33 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import annotation.*;
+@BOJ(   number = 2631,
+        tier = BaekjoonTier.GOLD_V,
+        solveDate = @SolveDate(year = 2022, month = 2 ,day = 21))
+
+public class BOJ2631 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int [] line = new int[N + 1];
+        int [] dp = new int[N + 1];
+        for (int n = 1; n <= N; n++) {
+            line[n] = Integer.parseInt(br.readLine());
+        }
+
+        int cnt = 0;
+        for (int n = 1; n <= N; n++) {
+            dp[n] = 1;
+            for (int i = 1; i <= N; i++) {
+                if (line[i] < line[n] && dp[n] < dp[i] + 1) dp[n] = dp[i] + 1;
+
+                if (cnt < dp[n]) cnt = dp[n];
+            }
+        }
+
+        System.out.println(cnt == N ? N : N - cnt);
+    }
+}
